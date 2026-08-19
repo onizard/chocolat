2aae7a6f4cc75e05bbfd2bc1c8a95b98c49b3f30
Olivier <ton@email>
2026-08-20 00:01:21 +0200

    maj

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rôle : rendre l'application utilisable sans connexion et satisfaire
    les critères d'installation de Chrome sur Android. */
 
-const CACHE = 'chocolat-v20';
+const CACHE = 'chocolat-v21';
 const COQUILLE = [
   './',
   './index.html',
